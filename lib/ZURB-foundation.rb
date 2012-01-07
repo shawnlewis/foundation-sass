2340a965ffd114153d38ce2df6cf5b69e53bafea
zurbchris <chris@zurb.com>
2012-01-07 15:18:10 -0800

    bumped version before pushing new gem

--- lib/ZURB-foundation.rb
+++ lib/ZURB-foundation.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("ZURB-foundation", :path => File.join(File.dirname(
 
 module ZURBfoundation
   
-  VERSION = "2.1.3.3"
-  DATE = "2011-12-21"
+  VERSION = "2.1.4"
+  DATE = "2012-01-07"
 
 end
