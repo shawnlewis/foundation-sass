7fd715037807d13b53aa0074a4305da15055640e
zurbchris <chris@zurb.com>
2012-01-26 21:14:50 -0800

    target orbit slides using class

--- templates/project/javascripts/jquery.orbit-1.3.0.js
+++ templates/project/javascripts/jquery.orbit-1.3.0.js
@@ -9,6 +9,13 @@
 
 (function($) {
   
+  $.fn.findFirstImage = function () {
+	  return this.first()
+	          .find('img')
+            .andSelf().filter('img')
+            .first();
+	};
+  
   var ORBIT = {
     
     defaults: {  
@@ -106,6 +113,8 @@
         .addClass('orbit')
         .css({width: '1px', height: '1px'});
         
+      this.$slides.addClass('orbit-slide');
+        
       this.setDimentionsFromLargestSlide();
       this.updateOptionsIfOnlyOneSlide();
       this.setupFirstSlide();
@@ -142,7 +151,8 @@
       self.$element.add(self.$wrapper).height(this.$slides.first().height());
       self.orbitWidth = this.$slides.first().width();
       self.orbitHeight = this.$slides.first().height();
-      $fluidPlaceholder = this.$slides.first().clone();
+      $fluidPlaceholder = this.$slides.first().findFirstImage().clone();
+        
       
       this.$slides.each(function () {
         var slide = $(this),
@@ -156,7 +166,7 @@
         if (slideHeight > self.$element.height()) {
           self.$element.add(self.$wrapper).height(slideHeight);
           self.orbitHeight = self.$element.height();
-          $fluidPlaceholder = $(this).clone();
+          $fluidPlaceholder = $(this).findFirstImage().clone();
 	      }
         self.numberSlides += 1;
       });
