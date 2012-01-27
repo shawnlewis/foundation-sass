97bde19deb2ec3dc340f788f40160e4c32d017b9
zurbchris <chris@zurb.com>
2012-01-26 21:37:31 -0800

    bumped version to 2.5.1

--- lib/ZURB-foundation.rb
+++ lib/ZURB-foundation.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("ZURB-foundation", :path => File.join(File.dirname(
 
 module ZURBfoundation
   
-  VERSION = "2.1.4.2"
-  DATE = "2012-01-23"
+  VERSION = "2.1.5"
+  DATE = "2012-01-26"
 
 end
