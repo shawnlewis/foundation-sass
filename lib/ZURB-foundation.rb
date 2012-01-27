52ea9232f9169dedd77d620541077be08e94d858
zurbchris <chris@zurb.com>
2012-01-26 22:30:08 -0800

    added settings file and import for it, doh

--- lib/ZURB-foundation.rb
+++ lib/ZURB-foundation.rb
@@ -3,7 +3,7 @@ Compass::Frameworks.register("ZURB-foundation", :path => File.join(File.dirname(
 
 module ZURBfoundation
   
-  VERSION = "2.1.5"
+  VERSION = "2.1.5.1"
   DATE = "2012-01-26"
 
 end
